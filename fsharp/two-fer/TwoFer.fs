b8a41652387a8204eab9e1c4d70fa44b92c9d95b
Elton Santana <elton.garcia.s@gmail.com>
2020-07-07 11:36:33 -0300

    Add fsharp folder

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let twoFer (input: string option): string = 
+    match input with
+    | None -> "One for you, one for me."
+    | name -> sprintf "One for %s, one for me." name.Value
