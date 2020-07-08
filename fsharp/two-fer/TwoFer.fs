9deff5f94ddb47e674abf78428f961bb927b6f0a
Elton Santana <elton.garcia.s@gmail.com>
2020-07-07 23:02:56 -0300

    Inprove two-fer solution

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,6 +1,4 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string = 
-    match input with
-    | None -> "One for you, one for me."
-    | name -> sprintf "One for %s, one for me." name.Value
+    sprintf "One for %s, one for me." (Option.defaultValue "you" input)
