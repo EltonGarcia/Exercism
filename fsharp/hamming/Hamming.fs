b5686fefcc6a3a750b0c2af51d4331611902d3bc
Elton Santana <elton.garcia.s@gmail.com>
2020-07-07 23:03:19 -0300

    Solve hamming

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,7 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option = 
+    if strand1.Length <> strand2.Length then None
+    else Seq.zip strand1 strand2 
+        |> Seq.sumBy (fun (a, b) -> if a <> b then 1 else 0)
+        |> Some
